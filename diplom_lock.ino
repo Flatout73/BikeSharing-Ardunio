c53f37c5cd1d696b91928d877f1d752c83d0fdb8
Flatout73 <fantom2259@gmail.com>
2019-06-01 22:32:59 +0300

    gps

--- diplom_lock.ino
+++ diplom_lock.ino
@@ -4,9 +4,9 @@
 
 SoftwareSerial BTSerial(6, 3);
 
-//SoftwareSerial GPSSerial(7, 2);
+SoftwareSerial GPSSerial(7, 2);
 
-//GPS gps(GPSSerial);
+GPS gps(GPSSerial);
 
 #define MAX_SIZE_MASS 16
 // массив для хранения текущего времени
@@ -37,12 +37,12 @@ void setup() {
   while(!Serial || !BTSerial);
   Serial.println("AT commands: okay");
 
-//  GPSSerial.begin(115200);
-//  Serial.println("GPS init is OK on speed 115200");
-//  GPSSerial.write("$PMTK251,9600*17\r\n");
-//  GPSSerial.end();
-//  GPSSerial.begin(9600);
-//  Serial.print("GPS init is OK on speed 9600\n");
+  GPSSerial.begin(115200);
+  Serial.println("GPS init is OK on speed 115200");
+  GPSSerial.write("$PMTK251,9600*17\r\n");
+  GPSSerial.end();
+  GPSSerial.begin(9600);
+  Serial.print("GPS init is OK on speed 9600\n");
 }
 
 void loop() {
@@ -84,64 +84,68 @@ void loop() {
     BTSerial.write(Serial.read());
 
       // если пришли данные с gps-модуля
-//  if (gps.available()) {
-//    // считываем данные и парсим
-//    gps.readParsing();
-//    // проверяем состояние GPS-модуля
-//    switch(gps.getState()) {
-//      // всё OK
-//      case GPS_OK:
-//        Serial.println("GPS is OK");
-//        // выводим координаты широты и долготы
-//        // 1. в градусах, минутах и секундах
-//        // 2. градусах в виде десятичной дроби
-//        Serial.println("GPS Coordinates: ");
-//        gps.getLatitudeBase60(latitudeBase60, MAX_SIZE_MASS);
-//        gps.getLongitudeBase60(longitudeBase60, MAX_SIZE_MASS);
-//        Serial.print("Latitude\t");
-//        Serial.print(latitudeBase60);
-//        Serial.print("\t\t");
-//        Serial.println(gps.getLatitudeBase10(), 6);
-//        Serial.print("Longitude\t");
-//        Serial.print(longitudeBase60);
-//        Serial.print("\t\t");
-//        Serial.println(gps.getLongitudeBase10(), 6);
-//        // выводим количество видимых спутников
-//        Serial.print("Sat: ");
-//        Serial.println(gps.getSat());
-//        // выводим текущую скорость
-//        Serial.print("Speed: ");
-//        Serial.println(gps.getSpeedKm());
-//        // выводим высоту над уровнем моря
-//        Serial.print("Altitude: ");
-//        Serial.println(gps.getAltitude());
-//        // выводим текущее время
-//        Serial.print("Time: ");
-//        gps.getTime(strTime, MAX_SIZE_MASS);
-//        gps.getDate(strDate, MAX_SIZE_MASS);
-//        Serial.write(strTime);
-//        Serial.println();
-//        // выводим текущую дату
-//        Serial.print("Date: ");
-//        Serial.write(strDate);
-//        Serial.println("\r\n");
-//        // каждую переменную дату и времени можно выводить отдельно
-//  /*    Serial.print(gps.getHour());
-//        Serial.print(gps.getMinute());
-//        Serial.print(gps.getSecond());
-//        Serial.print(gps.getDay());
-//        Serial.print(gps.getMonth());
-//        Serial.print(gps.getYear());
-//  */   
-//        break;
-//      // ошибка данных
-//      case GPS_ERROR_DATA:
-//        Serial.println("GPS error data");
-//        break;
-//      // нет соединение со спутниками
-//      case GPS_ERROR_SAT:
-//        //Serial.println("GPS no connect to satellites!!!");
-//        break;
-//    }
-//  }
+  if (gps.available()) {
+    // считываем данные и парсим
+    gps.readParsing();
+    // проверяем состояние GPS-модуля
+    switch(gps.getState()) {
+      // всё OK
+      case GPS_OK:
+        Serial.println("GPS is OK");
+        // выводим координаты широты и долготы
+        // 1. в градусах, минутах и секундах
+        // 2. градусах в виде десятичной дроби
+        Serial.println("GPS Coordinates: ");
+        gps.getLatitudeBase60(latitudeBase60, MAX_SIZE_MASS);
+        gps.getLongitudeBase60(longitudeBase60, MAX_SIZE_MASS);
+        Serial.print("Latitude\t");
+        Serial.print(latitudeBase60);
+        Serial.print("\t\t");
+        Serial.println(gps.getLatitudeBase10(), 6);
+        Serial.print("Longitude\t");
+        Serial.print(longitudeBase60);
+        Serial.print("\t\t");
+        Serial.println(gps.getLongitudeBase10(), 6);
+        // выводим текущее время
+        Serial.print("Time: ");
+        gps.getTime(strTime, MAX_SIZE_MASS);
+        gps.getDate(strDate, MAX_SIZE_MASS);
+        Serial.write(strTime);
+        Serial.println();
+        // выводим текущую дату
+        Serial.print("Date: ");
+        Serial.write(strDate);
+        Serial.println("\r\n");
+
+//        String coord = String("COORDINATES: ");
+//        coord.concat(gps.getLatitudeBase10());
+//        coord.concat(":");
+//        coord.concat(gps.getLongitudeBase10());
+//        char array[100];
+//        coord.toCharArray(array, 100);
+ //       BTSerial.print(coord);
+       // BTSerial.write(coor);
+
+       BTSerial.print("COORDINATES: ");
+       BTSerial.print(gps.getLatitudeBase10(), DEC);
+       BTSerial.print("\n");
+       BTSerial.print(gps.getLongitudeBase10(), DEC);
+
+        break;
+      // ошибка данных
+      case GPS_ERROR_DATA:
+        Serial.println("GPS error data");
+        break;
+      // нет соединение со спутниками
+      case GPS_ERROR_SAT:
+        Serial.println("GPS no connect to satellites!!!");
+        
+       BTSerial.print("@COORD: ");
+       BTSerial.print(55.6778989, DEC);
+       BTSerial.print(" ");
+       BTSerial.print(34.6788777, DEC);
+       BTSerial.print("@");
+        break;
+    }
+  }
 }
